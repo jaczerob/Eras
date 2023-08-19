f82203795bfe1a101baa91b1d94096aaf3741ab7
Jacob <thewallacems@gmail.com>
2023-08-19 13:21:57 -0400

    add in wss

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,7 +1,9 @@
+import { Buffer } from 'buffer';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Status } from "src/app/models/status";
 import { ToontownService } from "src/app/services/toontown.service";
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -31,6 +33,30 @@ export class LoginComponent {
   }
   
   onSubmit(): void {
-    open('toontown://')
+    if (this.username === '' || this.password === '') {
+      alert('Please enter a username and password.');
+      return;
+    }
+
+    const username = this.username
+    const password = this.password
+
+    const socket = new WebSocket(environment.wsUrl);
+    socket.onmessage = function (msg) {
+      console.log('message from ws: %s', msg.data)
+    }
+
+    socket.onclose = function (ev) {
+      console.log('socket connection closed')
+    }
+
+    socket.onopen = function (_) {
+      console.log('socket connection opened')
+
+      const encryptedDetails = Buffer.from(`${username}:${password}`).toString('base64')
+      socket.send(encryptedDetails)
+
+      socket.close()
+    }
   }
 }
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   baseUrl: 'https://api.jaczerob.dev',
+  wsUrl: 'wss://local.jaczerob.dev:4500/ws/toontown',
   headers: {
       'Content-Type': 'application/json'
   }
